cd8ff12f238c3d090be3eb12f72b6a4852a46447
AYaldaShabani <yalda.shabani27@gmail.com>
2024-09-29 14:18:28 +0330

    first commit

---
+++ src/screens/ContactUs/index.jsx
@@ -0,0 +1,9 @@
+const ContactUs = () => {
+    return (
+        <div>
+            {/* contact component holder in here , in figma : web1920-1*/}
+            </div>
+    )
+}
+
+export default ContactUs
---
+++ src/screens/Login/index.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div>
+            {/*Login holder in here */}
+        </div>
+    )
+}
+
+export default Login
